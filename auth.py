c11eb59cbc6c4d30bc616206b7d127d8c61f59b1
vasiliyaver <76477090+vasiliyaver@users.noreply.github.com>
2020-12-23 14:17:39 +0300

    Create auth.py

---
+++ auth.py
@@ -0,0 +1,28 @@
+# dictionary - словарь
+database = {
+    'vasaver':'abcde',
+    'annvas':'fdcba',
+    'vvas':'abdf',
+    'ahct':'fdba',
+
+}
+
+def check_password(login,password):
+    if login in database:
+        if database[login] == password:
+            return True
+    return False
+
+password_count = 0
+user_login = 'vikvas'
+
+# команда with open открывает файл сама и закрывает файл сама
+with open('pop-passwords.txt') as passwords_file:
+    for line in passwords_file:
+        user_password = line.strip()
+
+        if check_password(user_login,user_password) is True:
+            print(f'Попытка № {password_count}: пароль {user_password} подошел')
+            break
+        password_count += 1
+print(f'файл закрыт,{password_count=}')
